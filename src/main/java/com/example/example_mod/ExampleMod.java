c2b14e0ad08269f7a06438d6c218fd11dc56b565
Space Walker <spacedoesrs@gmail.com>
2023-07-17 00:04:08 +0200

    template for 1.3+ using fabric loader

---
+++ src/main/java/com/example/example_mod/ExampleMod.java
@@ -0,0 +1,13 @@
+package com.example.example_mod;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class ExampleMod {
+
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod name as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final Logger LOGGER = LogManager.getLogger("Example Mod");
+
+}
